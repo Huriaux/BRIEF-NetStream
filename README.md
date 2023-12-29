b7f730e8f7645988f74ab23b8d124ba78106b20b
Cecile HURIAUX <cecile.huriaux@gmail.com>
2023-12-29 17:09:55 +0100

    feat(NetStream) Correcting :

--- README.md
+++ README.md
@@ -1,77 +1,85 @@
 # BRIEF-NetStream
 
 
-REQUÊTE SQL :
+## REQUÊTE SQL :
 
 1) Les titres et dates de sortie des films du plus récent au plus ancien
+```sql
 SELECT title, release_dt FROM Movies
 ORDER BY release_dt DESC;
+```
 
 
 
 2) Les noms, prénoms et âges des acteurs/actrices de plus de 50 ans dans l'ordre alphabétique
+```sql
 -- (j'ai modifié l'âge de la consigne car je me suis aperçu trop tard que je n'ai aucun acteur de moins de 30 ans)
+
 SELECT last_name, first_name, age
 FROM (
   SELECT last_name, first_name, EXTRACT(YEAR FROM AGE(NOW(), birthday)) AS age
   FROM Actors
 ) AS actor_age
 WHERE age <= 50;
+```
 
 
 
 3) La liste des acteurs/actrices principaux pour un film donné
+```sql
 SELECT Movies.movie_id, Movies.title, Actors.actor_id, Actors.last_name, Actors.first_name
 FROM Movies
 INNER JOIN Actors ON Movies.actor_id = Actors.actor_id
 WHERE Movies.title = 'Deadpool';
-
+```
 
 
 4) La liste des films pour un acteur/actrice donné
+```sql
 SELECT Actors.actor_id, Actors.last_name, Actors.first_name, Movies.movie_id, Movies.title
 FROM Actors
 INNER JOIN Movies ON  Actors.actor_id = Movies.actor_id
 WHERE Actors.last_name = 'Reynolds';
+```
 
 
 
 5) Ajouter un film (avec une faute d'orthographe dans le nom)
+```sql
 -- ("Il faut sauver le soldat Ryan", "1998-07-24", "169 minutes", "Hanks", "Tom", "Captain John Miller", "1956-07-09", "Spielberg", "Steven")
 SELECT * FROM Movies;
 INSERT INTO Movies (Title, release_dt, duration, actor_id, director_id)
 	VALUES 	('Il faut sauver le soldat Bryan', '1998-07-24', 169, 7, 11);
+  ```
 
 
 
 6) Ajouter un acteur/actrice
+```sql
 SELECT * FROM Actors;
 INSERT INTO Actors (last_name, first_name, birthday) 
 VALUES
 	('Damon', 'Matt', '1970-10-08');
-
+```
 
 
 7) Modifier un film (--> Title : faute d'orthographe)
+```sql
 SELECT * FROM Movies;
 UPDATE Movies SET title = 'Il faut sauver le soldat Ryan' WHERE movie_id = 18;
-
+```
 
 
 8) Supprimer un acteur/actrice
+```sql
 select * from Actors;
 delete from Actors where actor_id = 16;
-
+```
 
 
 9) Afficher les 3 derniers acteurs/actrices ajouté(e)s
--- SELECT * FROM Actors;
--- INSERT INTO Actors (last_name, first_name, birthday) 
--- VALUES
--- 	('Jolie', 'Angelina', '1975-06-04'),
--- 	('Smith', 'Will', '1968-09-25'),
--- 	('Winslet', 'Kate', '1975-10-05');
-
+```sql
 SELECT actor_id, last_name, first_name, creation_dt FROM Actors
 ORDER BY creation_dt desc
 lLIMITimit 3;
+```
--- script.sql
+++ script.sql
@@ -4,13 +4,15 @@
 -- J'ai juger ma méthode plus clair, même si elle m'a donné plus de travail et de réflexion. 
 
 -- création table des Acteurs
-CREATE TABLE Actors (
+CREATE TABLE IF NOT EXISTS Actors (
 	actor_id SERIAL PRIMARY KEY,
-	last_name VARCHAR(100) NOT NULL,
-	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT null,
+	first_name VARCHAR(100) NOT null,
 	birthday DATE,
 	creation_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	modification_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	modification_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	-- Ajout de la contrainte d'unicité sur nom et prénom de l'acteur
+	UNIQUE (last_name, first_name)
 );
 
 -- Création fonction (trigger) => si ON AJOUTE des données
@@ -23,13 +25,13 @@ END;
 $create_date_actors$ LANGUAGE plpgsql; -- variable trigger + spécifie le langage
 
 --Création du trigger (en appelant la fonction 'set_creation_date()')
-CREATE TRIGGER set_creation_date_trigger
+CREATE trigger set_creation_date_trigger
 BEFORE INSERT ON Actors -- avant l'insertion dans la table Actors
 FOR EACH ROW -- pour chaque ligne
 EXECUTE FUNCTION set_creation_date(); -- exécute la fonction pour chaque nouvelle ligne insérée
 
 -- Ajout données
-INSERT INTO Actors (last_name, first_name, birthday) 
+INSERT INTO Actors (last_name, first_name, birthday)
 VALUES
 	('DiCaprio', 'Leonardo', '1974-11-11'),
 	('Gosling', 'Ryan', '1980-11-12'),
@@ -45,7 +47,9 @@ VALUES
 	('Foster', 'Jodie', '1962-11-19'),
 	('Eisenberg', 'Jesse', '1983-10-05'),
 	('De Niro', 'Robert', '1943-08-17'),
-	('Bogart', 'Humphrey', '1899-12-25');
+	('Bogart', 'Humphrey', '1899-12-25')
+-- clause ON CONFLICT pour éviter d'insérer des données déjà existantes
+ON CONFLICT (last_name, first_name) DO NOTHING;
 
 -- Création fonction (trigger) => si ON MODIFIE des données
 CREATE OR REPLACE FUNCTION set_modification_date()
@@ -65,16 +69,25 @@ EXECUTE FUNCTION set_modification_date(); -- exécute la fonction pour chaque no
 -- Afficher table
 SELECT * FROM Actors;
 
+-- + 3 (derniers) acteurs ajoutés avant les requêtes
+SELECT * FROM Actors;
+INSERT INTO Actors (last_name, first_name, birthday) 
+VALUES
+	('Jolie', 'Angelina', '1975-06-04'),
+	('Smith', 'Will', '1968-09-25'),
+	('Winslet', 'Kate', '1975-10-05');
+
 
 
 
 -- todo = [TABLE DE LIAISON]  'RÔLES' <---> 'ACTEURS'
 
 -- Création de la table relationnelle 'Roles'
-CREATE TABLE Roles (
+CREATE TABLE IF NOT EXISTS Roles (
 	role_id SERIAL PRIMARY KEY,
 	role_actor VARCHAR(100) NOT NULL,
-	actor_id INT NOT NULL REFERENCES Actors (actor_id));
+	actor_id INT NOT NULL REFERENCES Actors (actor_id)
+);
 
 -- ajout données
 INSERT INTO Roles (role_actor, actor_id) 
@@ -104,12 +117,13 @@ SELECT * FROM Roles;
 -- todo = TABLE  'DIRECTEURS'
 
 -- création table des Réalisateurs
-CREATE TABLE Directors (
+CREATE TABLE IF NOT EXISTS Directors (
 	director_id SERIAL PRIMARY KEY,
 	last_name VARCHAR(100) NOT NULL,
 	first_name VARCHAR(100) NOT NULL,
 	creation_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	modification_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	modification_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE (last_name, first_name)
 );
 
 -- Création fonction (trigger)
@@ -144,7 +158,8 @@ VALUES
 	('Demme', 'Jonathan'),
 	('Fincher', 'David'),
 	('Scorsese', 'Martin'),
-	('Curtiz', 'Michael');
+	('Curtiz', 'Michael')
+ON CONFLICT (last_name, first_name) DO NOTHING;
 
 -- Création fonction (trigger) => si ON MODIFIE des données
 CREATE OR REPLACE FUNCTION set_modification_date()
@@ -170,7 +185,7 @@ SELECT * FROM Directors;
 -- todo = TABLE  'FILMS'
 
 -- Création de la table Films
-CREATE TABLE Movies (
+CREATE TABLE IF NOT EXISTS Movies (
 	movie_id SERIAL PRIMARY KEY,
 	title VARCHAR(100) NOT NULL,
 	release_dt DATE NOT NULL,
@@ -178,7 +193,8 @@ CREATE TABLE Movies (
 	actor_id INT NOT NULL REFERENCES Actors (actor_id),
 	director_id INT NOT NULL REFERENCES Directors (director_id),
 	creation_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	modification_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	modification_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE (title)
 );
 
 -- Création fonction (trigger)
@@ -215,7 +231,8 @@ VALUES
 	('The Silence of the Lambs', '1991-02-14', 118, 12, 12),
 	('The Social Network', '2010-09-24', 120, 13, 13),
 	('Goodfellas', '1990-09-19', 146, 14, 14),
-	('Casablanca', '1942-11-26', 102, 15, 15);
+	('Casablanca', '1942-11-26', 102, 15, 15)
+ON CONFLICT (title) DO NOTHING;
 
 
 
@@ -243,7 +260,7 @@ SELECT * FROM Movies;
 -- todo = TABLE  'UTILISATEURS'
 
 -- Création de la table Utilisateurs
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
 	user_id SERIAL PRIMARY KEY,
 	first_name VARCHAR(100),
 	last_name VARCHAR(100),
@@ -252,7 +269,8 @@ CREATE TABLE Users (
 	role_user VARCHAR(50),
 	favorite_movies_list VARCHAR(100),
 	creation_dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	modification_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	modification_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE (email, password_user)
 );
 
 -- Création fonction (trigger)
@@ -292,7 +310,8 @@ VALUES
 	('Avery', 'Baker', 'avery.baker@email.com', 'pass456', 11, 'user'),
 	('Elijah', 'Fisher', 'elijah.fisher@email.com', 'abc123', 10, 'user'),
 	('Grace', 'Cooper', 'grace.cooper@email.com', 'qwerty', 14, 'user'),
-	('Liam', 'Reed', 'liam.reed@email.com', 'pass123', 14, 'user');
+	('Liam', 'Reed', 'liam.reed@email.com', 'pass123', 14, 'user')
+ON CONFLICT (email, password_user) DO NOTHING;
 
 -- Création fonction (trigger) => si ON MODIFIE des données
 CREATE OR REPLACE FUNCTION set_modification_date()
@@ -318,7 +337,7 @@ SELECT * FROM Users;
 -- todo = [TABLE DE LIAISON]  'FILMS REGARDÉS'  (movie_id <---> user_id)
 
 -- Création de la table relationnelle Watching
-CREATE TABLE Watching (
+CREATE TABLE IF NOT EXISTS Watching (
 	id SERIAL PRIMARY KEY,
 	movie_id INT REFERENCES Movies (movie_id),
 	user_id INT REFERENCES Users (user_id)
@@ -349,3 +368,84 @@ VALUES
 	(20, 6);
 
 SELECT * FROM Watching;
+
+
+-- readme.md
+
+# BRIEF-NetStream
+
+
+REQUÊTE SQL :
+
+1) Les titres et dates de sortie des films du plus récent au plus ancien
+SELECT title, release_dt FROM Movies
+ORDER BY release_dt DESC;
+
+
+
+2) Les noms, prénoms et âges des acteurs/actrices de plus de 50 ans dans l'ordre alphabétique
+-- (j'ai modifié l'âge de la consigne car je me suis aperçu trop tard que je n'ai aucun acteur de moins de 30 ans)
+SELECT last_name, first_name, age
+FROM (
+  SELECT last_name, first_name, EXTRACT(YEAR FROM AGE(NOW(), birthday)) AS age
+  FROM Actors
+) AS actor_age
+WHERE age <= 50;
+
+
+
+3) La liste des acteurs/actrices principaux pour un film donné
+SELECT Movies.movie_id, Movies.title, Actors.actor_id, Actors.last_name, Actors.first_name
+FROM Movies
+INNER JOIN Actors ON Movies.actor_id = Actors.actor_id
+WHERE Movies.title = 'Deadpool';
+
+
+
+4) La liste des films pour un acteur/actrice donné
+SELECT Actors.actor_id, Actors.last_name, Actors.first_name, Movies.movie_id, Movies.title
+FROM Actors
+INNER JOIN Movies ON  Actors.actor_id = Movies.actor_id
+WHERE Actors.last_name = 'Reynolds';
+
+
+
+5) Ajouter un film (avec une faute d'orthographe dans le nom)
+-- ("Il faut sauver le soldat Ryan", "1998-07-24", "169 minutes", "Hanks", "Tom", "Captain John Miller", "1956-07-09", "Spielberg", "Steven")
+SELECT * FROM Movies;
+INSERT INTO Movies (Title, release_dt, duration, actor_id, director_id)
+	VALUES 	('Il faut sauver le soldat Bryan', '1998-07-24', 169, 7, 11);
+
+
+
+6) Ajouter un acteur/actrice
+SELECT * FROM Actors;
+INSERT INTO Actors (last_name, first_name, birthday) 
+VALUES
+	('Damon', 'Matt', '1970-10-08');
+
+
+
+7) Modifier un film (--> Title : faute d'orthographe)
+SELECT * FROM Movies;
+UPDATE Movies SET title = 'Il faut sauver le soldat Ryan' WHERE movie_id = 18;
+
+
+
+8) Supprimer un acteur/actrice
+select * from Actors;
+delete from Actors where actor_id = 16;
+
+
+
+9) Afficher les 3 derniers acteurs/actrices ajouté(e)s
+-- SELECT * FROM Actors;
+-- INSERT INTO Actors (last_name, first_name, birthday) 
+-- VALUES
+-- 	('Jolie', 'Angelina', '1975-06-04'),
+-- 	('Smith', 'Will', '1968-09-25'),
+-- 	('Winslet', 'Kate', '1975-10-05');
+
+SELECT actor_id, last_name, first_name, creation_dt FROM Actors
+ORDER BY creation_dt desc
+lLIMITimit 3;
